0a6f044461c0c29a2e467af2a6b6144c49f2f8b5
DanivonneCaSan <daniela.castillo.sn@gmial.com>
2018-02-16 08:59:47 -0600

    Modificando funcionalidad

--- js/app.js
+++ js/app.js
@@ -9,6 +9,10 @@ $(document).ready(function() {
 
 });
 
+function loadPage() {
+  $("#searchbtn1").click(optionsMenu);
+}
+
 function initMap() {
   var uluru = {
     lat: 19.4279937,
@@ -24,21 +28,94 @@ function initMap() {
   });
 }
 
-function search() {
-  // Declare variables
-  var input, filter, ul, li, a, i;
-  input = document.getElementById('search');
-  // filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+function printRestaurants(restaurant) {
+  // crear elementos
+  var $div = $("<div />", {
+    "class": "rest-cont"
+  });
+  var $restaurantName = $("<h2 />");
+  var $restaurantAdress = $("<p />");
+  var $restaurantDescrip = $("<p />");
+  var $restaurantPrice = $("<p />");
+  var $div1 = $("<div />", {
+    "class": "img-cont"
+  });
+  var $restaurantImg = $("<img />", {
+    "class": "img-thumbnail sizeimg"
+  });
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
+  //  atributos y eventos a los elementos
+  $div.attr('data-toggle', 'modal');
+  $div.attr('data-target', '.bs-example-modal-sm');
+  // $div.addClass('pretty-div');
+  $restaurantImg.addClass('img-responsive');
+
+  // Extracción de info de la data
+  $restaurantName.text(restaurant.name);
+  $restaurantAdress.text(restaurant.adress);
+  $restaurantDescrip.text(restaurant.description);
+  $restaurantPrice.text(restaurant.price);
+  $restaurantImg.attr("src", restaurant.photo);
+
+  $div.append($restaurantName);
+  $div.append($restaurantAdress);
+  $div.append($restaurantDescrip);
+  $div.append($restaurantDescrip);
+  $div.append($restaurantPrice);
+  $div1.append($restaurantImg);
+  $div.append($div1);
+
+  // agregamos al contenedor de food
+  $("#cont-food").prepend($div);
+
+
 }
+
+function printRestaurantsModal(restaurant) {
+  // crear elementos
+  // Print modal
+  var $nameModal = $("<p />");
+  var $adressModal = $("<p />");
+  var $descriptModal = $("<p />");
+  var $priceModal = $("<p />");
+  // Modal Data
+  $nameModal.text(restaurant.name);
+  $adressModal.text(restaurant.adress);
+  $descriptModal.text(restaurant.description);
+  $priceModal.text(restaurant.price);
+
+  // Agregando al Modal
+  $("#name-mod").append($nameModal);
+  $("#adress-mod").append($adressModal);
+  $("#descript-mod").append($descriptModal);
+  $("#price-mod").append($priceModal);
+
+}
+
+//Función de filtro
+
+function optionsMenu() {
+  var searchOption = $("#search").val().toLowerCase();
+  if ($("#search").val().trim().length > 0) {
+    var filteredData = data.filter(function(restaurant) {
+      console.log(restaurant);
+      return restaurant.type.toLowerCase().indexOf(searchOption) >= 0;
+    });
+    // Limpiando el div cont-food
+    $("#cont-food").empty();
+    filteredData.forEach(function(restaurant) {
+      printRestaurants(restaurant);
+      printRestaurantsModal(restaurant);
+    });
+  } else {
+    $("#cont-food").empty();
+    data.forEach(function(restaurant) {
+      printRestaurants(restaurant);
+    });
+  }
+
+  console.log(optionsMenu);
+
+}
+
+$(document).ready(loadPage);
--- js/data.js
+++ js/data.js
@@ -1,167 +1,113 @@
-var data = {
-
-  "Fastfood": {
-    "food": [{
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-
-    ]
+var data = [{
+    "name": "Hamburguesas",
+    "type": "fastfood",
+    "photo": "http://dummyimage.com/165x131.png/FE2E2E/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$$"
   },
-
-  "Antojitos": {
-    "food": [{
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-
-    ]
+  {
+    "name": "Pizza Hot",
+    "type": "fastfood",
+    "photo": "http://dummyimage.com/165x131.png/F7FE2Effffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$"
   },
-
-  "Corrida": {
-    "food": [{
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-
-    ]
+  {
+    "name": "Hochos y más",
+    "type": "fastfood",
+    "photo": "http://dummyimage.com/165x131.png/FF8000/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$$"
   },
-
-  "Cafetería": {
-    "food": [{
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-      {
-        "name": "Non Associates",
-        "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
-        "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
-        "adress": "547-142 Magna Avda."
-      },
-
-    ]
-  }
-
-};
+  {
+    "name": "El Wen Comer",
+    "type": "corrida",
+    "photo": "http://dummyimage.com/165x131.png/00FF40/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$"
+  },
+  {
+    "name": "Fonda Margarita",
+    "type": "corrida",
+    "photo": "http://dummyimage.com/165x131.png/B40486/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$$"
+  },
+  {
+    "name": "Comidita Casera",
+    "type": "corrida",
+    "photo": "http://dummyimage.com/165x131.png/013ADF/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$"
+  },
+  {
+    "name": "Los chicos",
+    "type": "antojitos",
+    "photo": "http://dummyimage.com/165x131.png/FE2E64/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$"
+  },
+  {
+    "name": "Antojitos de Rafa",
+    "type": "antojitos",
+    "photo": "http://dummyimage.com/165x131.png/00BFFF/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$"
+  },
+  {
+    "name": "Sopes Lupe",
+    "type": "antojitos",
+    "photo": "http://dummyimage.com/165x131.png/ff4444/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$"
+  },
+  {
+    "name": "Las tías",
+    "type": "antojitos",
+    "photo": "http://dummyimage.com/165x131.png/FF0040/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$"
+  },
+  {
+    "name": "PanEnvía",
+    "type": "cafeteria",
+    "photo": "http://dummyimage.com/165x131.png/5858FA/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$$"
+  },
+  {
+    "name": "Coffe Lovers",
+    "type": "cafeteria",
+    "photo": "http://dummyimage.com/165x131.png/04B404/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$$"
+  },
+  {
+    "name": "Sorry",
+    "type": "cafeteria",
+    "photo": "http://dummyimage.com/165x131.png/B40404/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$$"
+  },
+  {
+    "name": "Ladies Room",
+    "type": "cafeteria",
+    "photo": "http://dummyimage.com/165x131.png/5858FA/ffffff",
+    "description": "Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+    "adress": "547-142 Magna Avda.",
+    "price": "$$$"
+  },
+];
